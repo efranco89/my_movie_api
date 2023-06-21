b1aeae16cb4faf59b4a42388f52fdc7721694dd8
Enrique Franco <luis.franco@wizeline.com>
2023-06-21 15:53:44 -0500

    The GET method has been improved

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+app.title = "Mi aplicacion con FastAPI"
+app.version = "0.0.1"
 
-@app.get("/")
+movies = [
+  {
+    "id": 1, 
+    "title": "Avatar",
+    "overview": "Quis officia laborum anim ipsum cupidatat exercitation sint deserunt anim labore magna exercitation excepteur commodo.",
+    "year": 2009,
+    "rating": 7.8,
+    "category": "Action"
+  }
+]
+
+@app.get("/", tags=['home'])
 def message():
-  return "Hello World From docker container does it reload? I added the reload instruction to uvicorn"
+  return HTMLResponse("<h1> Hello World </h1>")
+
+@app.get("/movies", tags=['movies'])
+def get_movies():
+  return movies
