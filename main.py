a2f24539f1ed78eccb577f55808f3cbe6c5811c6
Enrique Franco <luis.franco@wizeline.com>
2023-06-21 21:32:50 -0500

    The JWT authentication was added

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from fastapi import FastAPI, Body, Path, Query
+from fastapi import FastAPI, Body, Path, Query, Request, HTTPException, Depends
 from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.security.http import HTTPAuthorizationCredentials
 from pydantic import BaseModel, Field
 from typing import Optional, List
+from starlette.requests import Request
+from jwt_manager import create_token, validate_token
+from fastapi.security import HTTPBearer
 
 app = FastAPI()
 app.title = "Mi aplicacion con FastAPI"
 app.version = "0.0.1"
+
+class JWTBearer(HTTPBearer):
+  async def __call__(self, request: Request):
+    auth = await super().__call__(request)
+    data = validate_token(auth.credentials)
+    if data['email'] != "admin@gmail.com":
+      raise HTTPException(status_code=403, detail="Invalid Token")
+    
+class User(BaseModel):
+  email: str
+  password: str
 
 class Movie(BaseModel):
   id: Optional[int] = None
@@ -50,28 +65,37 @@ movies = [
 def message():
   return HTMLResponse("<h1> Hello World </h1>")
 
-@app.get("/movies", tags=['movies'], response_model=List[Movie])
-def get_movies() -> List[Movie]:
-  return JSONResponse(content= movies)
+@app.post("/login", tags=['auth'])
+def login(user: User):
+  if user.email == 'admin@gmail.com' and user.password == "admin":
+    token: str = create_token(user.dict())
+    return JSONResponse(status_code= 200, content=token)
 
-@app.get('/movies/{id}', tags=['movies'], response_model=Movie)
+@app.get("/movies", tags=['movies'], response_model=List[Movie], status_code=200, dependencies=[Depends(JWTBearer())])
+def get_movies() -> List[Movie]:
+  return JSONResponse(status_code=200, content= movies)
+
+@app.get('/movies/{id}', tags=['movies'], response_model=Movie, status_code=200)
 def get_movie(id: int = Path(ge=1, le=2000)) -> Movie:
   for movie in movies:
     if movie["id"] == id:
-      return JSONResponse(content=movie)
-  return JSONResponse(content=[])
+      return JSONResponse(status_code= 200, content=movie)
+  return JSONResponse(status_code=404, content=[])
     
-@app.get("/movies/", tags=['movies'], response_model=List[Movie])
+@app.get("/movies/", tags=['movies'], response_model=List[Movie], status_code=200)
 def get_movies_by_category(category: str = Query(min_length=5, max_length=15))-> List[Movie]:
   movie = list(filter(lambda movie : movie['category']== category, movies))
-  return JSONResponse(content=movie)
+  if len(movie):
+    return JSONResponse(status_code=200, content=movie)
+  else:
+    return JSONResponse(status_code=404, content=movie)
 
-@app.post('/movies', tags=['movies'], response_model=dict)
+@app.post('/movies', tags=['movies'], response_model=dict, status_code=201)
 def create_movies(movie: Movie) -> dict:
   movies.append(movie)
-  return JSONResponse(content={"message": "The movie has been created"})
+  return JSONResponse(status_code=201, content={"message": "The movie has been created"})
 
-@app.put('/movies/{id}', tags=['movies'], response_model=dict)
+@app.put('/movies/{id}', tags=['movies'], response_model=dict, status_code=200)
 def update_movie(id: int, movie: Movie) -> dict:
   for item in movies: 
     if item["id"] == id: 
@@ -80,11 +104,13 @@ def update_movie(id: int, movie: Movie) -> dict:
       item['year'] = movie.year
       item['rating'] = movie.rating
       item['category'] = movie.category
-  return JSONResponse(content={"message": "The movie has been updated"})
+  return JSONResponse(status_code=200,content={"message": "The movie has been updated"})
 
-@app.delete('/movies/{id}', tags=['movies'], response_model=dict)
+@app.delete('/movies/{id}', tags=['movies'], response_model=dict, status_code=200)
 def delete_movie(id: int) -> dict:
   for movie in movies: 
     if movie["id"] == id:
       movies.remove(movie)
-  return JSONResponse(content={"message": "The movie has been deletes"})
+      return JSONResponse(status_code=200, content={"message": "The movie has been deleted"})
+  return JSONResponse(status_code=404, content={})
+  
